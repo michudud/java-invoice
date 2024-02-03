dada4bc01f33ac26e03d93ab47751f035aaea4c2
midudzik <midudzik@d17-427-pc-17.ipa.ii.agh.edu.pl>
2024-02-03 12:14:42 +0100

    passed all tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -8,32 +8,34 @@ import java.util.Map;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private HashMap<Product, Integer> products;
+    private HashMap<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
-        if(!products.containsKey(product)) {
-            this.products.put(product, 1);
-        }else{
-            this.products.put(product,products.get(product)+1);
-        }
+        if(product == null) throw new IllegalArgumentException();
+            if (!products.containsKey(product)) {
+                products.put(product, 1);
+            } else {
+                products.put(product, products.get(product) + 1);
+            }
     }
 
     public void addProduct(Product product, Integer quantity) {
+        if(quantity <= 0 || product == null) throw new IllegalArgumentException();
         if(!products.containsKey(product)) {
-            this.products.put(product, quantity);
+            products.put(product, quantity);
         }else{
-            this.products.put(product,products.get(product)+quantity);
+            products.put(product,products.get(product)+quantity);
         }
     }
 
     public BigDecimal getSubtotal() {
         BigDecimal subTotal = BigDecimal.ZERO;
 
-        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
-            Product product = entry.getKey();
-            Integer qty = entry.getValue();
-            subTotal.add(product.getPrice().multiply(BigDecimal.valueOf(qty)));
-        }
+            for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+                Product product = entry.getKey();
+                Integer qty = entry.getValue();
+                subTotal = subTotal.add(product.getPrice().multiply(BigDecimal.valueOf(qty)));
+            }
 
         return subTotal;
     }
@@ -41,23 +43,23 @@ public class Invoice {
     public BigDecimal getTax() {
         BigDecimal tax = BigDecimal.ZERO;
 
-        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
-            Product product = entry.getKey();
-            Integer qty = entry.getValue();
-            tax.add(product.getPrice().multiply(product.getTaxPercent()).multiply(BigDecimal.valueOf(qty)));
-        }
-
+            for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+                Product product = entry.getKey();
+                Integer qty = entry.getValue();
+                tax = tax.add(product.getPrice().multiply(product.getTaxPercent()).multiply(BigDecimal.valueOf(qty)));
+            }
+        //}
         return tax;
     }
 
     public BigDecimal getTotal() {
         BigDecimal total = BigDecimal.ZERO;
 
-        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
-            Product product = entry.getKey();
-            Integer qty = entry.getValue();
-            total.add(product.getPriceWithTax().multiply(BigDecimal.valueOf(qty)));
-        }
+            for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+                Product product = entry.getKey();
+                Integer qty = entry.getValue();
+                total = total.add(product.getPriceWithTax().multiply(BigDecimal.valueOf(qty)));
+            }
 
         return total;
     }
